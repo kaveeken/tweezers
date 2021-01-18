5f6e5e6036a5d6f1ac32c2bb9484797fa4bc2861
Kris <k.a.veeken@student.rug.nl>
2021-01-18 13:34:01 +0100

    find_unfold appears to work for this dataset

--- find_events.py
+++ find_events.py
@@ -1,6 +1,7 @@
 import numpy as np
 import lumicks.pylake as lk
 from util import load_estimates, extract_estimates, average_around
+from matplotlib import pyplot as plt
 
 def run_average(data, N):
     cumsum = np.cumsum(data)
@@ -8,34 +9,71 @@ def run_average(data, N):
     averages = (cumsum[N:] - cumsum[:-N]) / N
     return np.concatenate([prep, averages])
 
+
 def find_turnaround(data, N):
     r_avg = run_average(data, N)
     for i in range(N,len(r_avg)):
         if r_avg[i] < r_avg[i - 1]:
             return i
 
+
 def get_leg_length(f):
     for i in range(len(f)):
-        if force >= max(f) / 4:
-            return i
+        if f[i] >= max(f) / 4:
+            return round(i / 2)
 
-def forward_test(fit, model, landing, f):
-    sim = model(landing, fit)
-    if sim.std() <= fit.sigma[0]: # wrong
+
+def sort_by_dist(f, d):
+    d_sort = sorted(d) # maybe not sort twice
+    f_sort = [force for dist, force in sorted(zip(d,f))]
+    return f_sort, d_sort
+
+
+def search_unfold(f,d,model,est,leg_length,leg_last):
+    fit = lk.FdFit(model)
+    fit[model].add_data("prefold", f[0:leg_last], d[0:leg_last])
+    load_estimates(fit,est)
+    fit.fit()
+
+    target = leg_last + leg_length
+    sim = model(d[0:target], fit)
+    plt.scatter(d[0:leg_last], f[0:leg_last])
+    plt.scatter(d[leg_last:target], f[leg_last:target], c="tab:orange")
+    plt.scatter(d[0:target], sim, c="tab:green")
+    plt.show()
+    force_mean = average_around(f, target)["mean"]
+    if force_mean > sim[-1] + 2 * fit.sigma[0]:
+        # this is a strange case and probably unlikely
+        # - this actually happens a lot b/c test being too stringent
+        print("F much larger than simulated")
+        print(target)
         return False
+        #return 1
+    elif force_mean < sim[-1] - 2 * fit.sigma[0]:
+        first = target
+        for i in reversed(range(leg_last, target)):
+            if average_around(f, i)["mean"] < sim[i] -2 * fit.sigma[0]:
+                first = i
+        return round((first + target) / 2)
     else:
-        return True
+        return False
+    
 
 def find_unfold(f, d, model, est):
-    #fit = lk.FdFit(model)
     leg_length = get_leg_length(f)
     leg_last = leg_length
-    searching = True
-    while searching:
-        fit = lk.FdFit(model)
-        fit[model].add_data("prefold" , f[0:leg_last], d[0:leg_last])
-        load_estimates(fit, est)
-        fit.fit()
+    found = False
+    while not found:
+        found = search_unfold(f,d,model,est,leg_length,leg_last)
         leg_last += leg_length
-        landing = np.linspace(average_around(leg_last - 100, d)["mean"],
-                              average_around(leg_last, d)["mean"], 100)
+        if leg_last > len(f):
+            found = -2
+
+    if found < 0: # this needs to be proper error handling
+        print("something went wrong:")
+        if found == -1:
+            print("something strange")
+        elif found == -2:
+            print("no unfolding event found")
+
+    return found
--- scratch.py
+++ scratch.py
@@ -3,8 +3,7 @@ import matplotlib.pyplot as plt
 import lumicks.pylake as lk
 from copy import deepcopy
 from util import load_estimates, extract_estimates, average_around
-from find_unfold import forward_test
-from find_events import run_average, find_turnaround
+from find_events import run_average, find_turnaround, find_unfold
 
 file = lk.File("Data/adk5_curve1.h5")
 list(file.fdcurves)
@@ -23,7 +22,7 @@ composite_model_as_function_of_force = lk.odijk("dna_handles") \
 composite_model = composite_model_as_function_of_force.invert(interpolate=True,
                                                               independent_min=0,
                                                               independent_max=90) \
-                                    + lk.force_offset("dna_handles")
+                                  + lk.force_offset("dna_handles")
 fit = lk.FdFit(handles_model, composite_model)
 
 model2 = handles_model
@@ -37,8 +36,16 @@ fit["dna_handles/f_offset"].upper_bound = 6
 fit["dna_handles/f_offset"].lower_bound = -6
 fit.fit()
 
+estimates = extract_estimates(fit)
+
 print(extract_estimates(fit))
-print(forward_test(fit, handles_model, np.linspace(0.3,0.35,100),fdata))
+print("#############################################~")
+fit2 = lk.FdFit(handles_model, composite_model)
+fit2[handles_model].add_data("closed", fdata[0:1600],
+                            ddata[0:1600])
+print(fit2)
+load_estimates(fit2, extract_estimates(fit))
+print(extract_estimates(fit2))
 
 plt.figure()
 fit[handles_model].plot()
@@ -77,4 +84,23 @@ plt.plot(ddata)
 plt.plot(run_average(ddata,100))
 plt.plot(run_average(ddata,200))
 plt.axvline(x=find_turnaround(ddata,200))
+#plt.show()
+plt.close()
+
+unf = find_unfold(fdata[0:2000], ddata[0:2000], handles_model, estimates)
+
+print(unf)
+plt.scatter(ddata[0:1600],fdata[0:1600])
+plt.scatter(ddata[1600:2000],fdata[1600:2000],c="tab:red")
+plt.scatter(ddata[2000:2800],fdata[2000:2800],c="tab:orange")
+plt.scatter(ddata[2800:3200],fdata[2800:3200],c="tab:green")
+plt.scatter(ddata[3200:3500],fdata[3200:3500],c="tab:blue")
+plt.axvline(x=ddata[unf])
+plt.scatter(ddata[0:2000],handles_model(ddata[0:2000], fit))
+plt.savefig("scatter.png")
 plt.show()
+
+sim = handles_model(0.2,fit)
+print(sim)
+
+
